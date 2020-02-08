804e43bfba10b85409582ed050b9517e2f51ddc6
charles 🔥 <charleschong@Charless-MacBook-Pro.local>
2020-02-08 15:18:12 -0800

    remove panels

--- src/app/main/apps/academy/AcademyAppConfig.js
+++ src/app/main/apps/academy/AcademyAppConfig.js
@@ -3,12 +3,30 @@ import {Redirect} from 'react-router-dom';
 
 export const AcademyAppConfig = {
     settings: {
-        layout: {}
+        layout: {
+            config: {
+                navbar        : {
+                    display: true
+                },
+                toolbar       : {
+                    display: true
+                },
+                footer        : {
+                    display: false
+                },
+                leftSidePanel : {
+                    display: true
+                },
+                rightSidePanel: {
+                    display: false
+                }
+            }
+        }
     },
     routes  : [
         {
             path     : '/apps/academy/courses/:courseId/:courseHandle?',
-            component: React.lazy(() => import('./course/Course'))
+            component: React.lazy(() => import('./securethebox/course/Course'))
         },
         {
             path     : '/apps/academy/courses',
