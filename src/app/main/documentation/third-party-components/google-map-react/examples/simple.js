cfea01d38514d7c0327eaccd7ea4e6d6d1f5a06d
charles 🔥 <charleschong@Charless-MacBook-Pro.local>
2020-02-11 23:47:43 -0800

    remove google-map-react

--- src/app/main/documentation/third-party-components/google-map-react/examples/simple.js
+++ src/app/main/documentation/third-party-components/google-map-react/examples/simple.js
@@ -1,23 +1,23 @@
-import Icon from '@material-ui/core/Icon';
-import Tooltip from '@material-ui/core/Tooltip';
+// import Icon from '@material-ui/core/Icon';
+// import Tooltip from '@material-ui/core/Tooltip';
 import Typography from '@material-ui/core/Typography';
-import GoogleMap from 'google-map-react';
+// import GoogleMap from 'google-map-react';
 import React from 'react';
 
-function Marker({ text }) {
-	return (
-		<Tooltip title={text} placement="top">
-			<Icon className="text-red">place</Icon>
-		</Tooltip>
-	);
-}
+// function Marker({ text }) {
+// 	return (
+// 		<Tooltip title={text} placement="top">
+// 			<Icon className="text-red">place</Icon>
+// 		</Tooltip>
+// 	);
+// }
 
 function SimpleExample() {
 	return (
 		<div className="w-full">
 			<Typography className="h2 mb-16">Simple Map Example</Typography>
 			<div className="w-full h-512">
-				<GoogleMap
+				{/* <GoogleMap
 					bootstrapURLKeys={{
 						key: process.env.REACT_APP_MAP_KEY
 					}}
@@ -25,7 +25,7 @@ function SimpleExample() {
 					defaultCenter={[-34.397, 150.64]}
 				>
 					<Marker text="Marker Text" lat="-34.397" lng="150.644" />
-				</GoogleMap>
+				</GoogleMap> */}
 			</div>
 		</div>
 	);
