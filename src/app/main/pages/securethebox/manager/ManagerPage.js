3231f7c68dd9e9eb1788b6b547121701034cf6e3
charles 🔥 <charleschong@Charless-MacBook-Pro.local>
2020-02-29 07:13:27 -0800

    removed unused imports

--- src/app/main/pages/securethebox/manager/ManagerPage.js
+++ src/app/main/pages/securethebox/manager/ManagerPage.js
@@ -1,6 +1,5 @@
-import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
-import React, { useRef, useState } from 'react'
+import React from 'react'
 import 'react-typist/dist/Typist.css';
 import {
 	Environment,
